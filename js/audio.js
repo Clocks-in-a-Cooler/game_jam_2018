367acd5793a1f8fecef57a0f43b00883477dc69b
Luke Castellan <lljulianllwill@gmail.com>
2018-12-01 14:29:21 -0500

    Audio of Procyon
    Loads audio.js in index
    audio objects in index
    mp3 versions of tracks
    TODO update 4004

--- js/audio.js
+++ js/audio.js
@@ -1,14 +1,18 @@
 var menuTheme = document.getElementById("menuTrack");
 var explore = document.getElementById("exploreTrack");
 var dysonDiscovery = document.getElementById("dysonDiscoveryTrack");
-var cityBuilding = document.getElementById("paperworkTrack");
+//var cityBuilding = document.getElementById("paperworkTrack");
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function playTrack(soundTrack) {
-	console.log("current track is " + soundTrack);
+	soundTrack.loop = false;
+	soundTrack.play();
+}
+
+function playLoopTrack(soundTrack) {
 	soundTrack.loop = true;
 	soundTrack.play();
 }
@@ -19,5 +23,4 @@ async function pauseAudio(soundTrack) {
 		audioOST.volume = i/100;
 	}
 	soundTrack.pause();
-	console.log("audio paused at " + soundTrack.currentTime);
 }
