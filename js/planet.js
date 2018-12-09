ae61f8495072f5b9e4e0f3900f0fc89f8eb24050
clocks-in-a-cooler <>
2018-12-08 23:25:14 -0500

    did some work on the planet's file

--- js/planet.js
+++ js/planet.js
@@ -1,12 +1,25 @@
-function Planet(star, orbit_radius) {
-    this.star = star;
+function Planet(star, orbit_radius, sprite) {
+    this.star  = star;
+    this.angle = Math.random() * 2 * Math.PI;
     
-    this.orbit_raduis = orbit_radius;
+    this.orbit_radius = orbit_radius;
+    //this part needs reworking to follow Kepler's third law
+    this.orbit_speed  = 1e-6; //also 0.000001
     
+    //now calculate its position
+    this.x = Math.cos(angle) * this.orbit_radius + this.star.x;
+    this.y = Math.sin(angle) * this.orbit_radius + this.star.y;
 }
 
 Planet.prototype.radius = 100;
 
 Planet.prototype.get_new_position = function(lapse) {
+    angle += orbit_speed * lapse;
     
+    this.x = Math.cos(angle) * this.orbit_radius + this.star.x;
+    this.y = Math.sin(angle) * this.orbit_radius + this.star.y;
+};
+
+Planet.prototype.draw = function(context) {
+    context.drawImage(this.sprite, relative.x(this.x - this.radius), relative.y(this.y - this.radius));
 };
